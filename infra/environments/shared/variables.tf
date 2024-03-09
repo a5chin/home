39f68cdc28120caf62c3df5e0d77daa5c7a81293
eigo-hara <eigo.hara@dena.com>
2024-03-09 18:38:44 +0900

    create: artifact registry

---
+++ infra/environments/shared/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type        = string
+  description = "The ID of the Project."
+}
+
+variable "location" {
+  type        = string
+  default     = "us-central1"
+  description = "The location of resources."
+}
